374a9fa32745f25cac41bb6389a5847aaa4e2ca9
Natalie-Cardot <nataliecardot@gmail.com>
2021-04-18 13:50:01 -0700

    Edit comments

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function shadow(e) {
   let { clientX: x, clientY: y } = e;
   // let { offsetX: x, offsetY: y } = e;
 
-  // No longer necessary since changed to use clientX and clientY
+  // If check below no longer necessary since changed to use clientX and clientY
   // clientX/clientY: Gives us coordinates relative to top left corner of browser window (what we want)
-  // offsetX/offsetY: Give us coordinates relative to top left corner of parent element
+  // offsetX/offsetY: Give us coordinates relative to top left corner of target element
   // // Despite listening to mousemove on hero, if there are children elements being hovered over, will get x and y of those elements; must do normalization - if thing being hovered over is h1 instead of hero, modify x and y values so they're consistent
   // // e.target is thing it triggered on, whereas this is thing being listened on (hero)
   // if (this !== e.target) {
